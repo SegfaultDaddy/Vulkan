aec811c41f8c5bc67b509d2caea6cb9af95b22b9
SegfaultDaddy <sashagoodguy@gmail.com>
2024-08-16 18:09:26 +0300

    working create_swap_chain

--- source/triangle.cpp
+++ source/triangle.cpp
@@ -442,5 +442,37 @@ namespace app
         auto surfaceFormat{choose_swap_surface_format(swapChainSupport.formats)};
         auto presentMode{choose_swap_present_mode(swapChainSupport.presentModes)};
         auto extent{choose_swap_extent(swapChainSupport.capabilities)};
+
+        auto imageCount{swapChainSupport.capabilities.minImageCount + 1};
+        if(swapChainSupport.capabilities.maxImageCount > 0 &&
+           imageCount > swapChainSupport.capabilities.maxImageCount)
+        {
+            imageCount = swapChainSupport.capabilities.maxImageCount;
+        }
+
+        VkSwapchainCreateInfoKHR createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+        createInfo.surface = surface;
+        createInfo.minImageCount = imageCount;
+        createInfo.imageFormat = surfaceFormat.format;
+        createInfo.imageExtent = extent;
+        createInfo.imageArrayLayers = 1;
+        createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+        auto indices{find_queue_families(physicalDevice)};
+        std::vector<std::uint32_t> queueFamilyIndices{indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+        if(indices.graphicsFamily != indices.presentFamily)
+        {
+            createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+            createInfo.queueFamilyIndexCount = 2;
+            createInfo.pQueueFamilyIndices = std::data(queueFamilyIndices);
+        }
+        else
+        {
+            createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+            createInfo.queueFamilyIndexCount = 0;
+            createInfo.pQueueFamilyIndices = nullptr;
+        }
     }
 } 
